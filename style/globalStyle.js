fb910bf9d98b5afedb747ee80697b0927c869577
Lmineck <88527089+Lmineck@users.noreply.github.com>
2024-11-22 10:43:23 +0900

    Edit globalStyle.js

--- style/globalStyle.js
+++ style/globalStyle.js
@@ -7,7 +7,7 @@ const mobileMenuStyle = `m-0 block py-4 px-6`;
 // blog style과 notebook style
 const posth1Style = `text-[40px] font-bold mb-4 mt-6 border-b border-graylv2 pb-2.5`;
 const posth2Style = `text-[32px] font-bold mb-4 mt-6 border-b border-graylv2 pb-2.5`;
-const posth3Style = `text-[28px] font-bold mb-4 mt-6 border-b border-graylv2 pb-2.5`;
+const posth3Style = `text-[28px] font-bold mb-4 mt-6 pb-2.5`;
 const posth4Style = `text-2xl font-bold mb-2 mt-4`;
 const posth5Style = `text-xl font-bold mb-2 mt-4`;
 const posth6Style = `text-lg font-bold mb-2 mt-4`;
